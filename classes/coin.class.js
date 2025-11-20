035026559140ee8b7e52f3dd6bfd8641ff89a2b7
danielkrafzik <daniel.krafzik@hotmail.de>
2025-11-20 21:58:53 +0100

    added coin animation

--- classes/coin.class.js
+++ classes/coin.class.js
@@ -1,10 +1,28 @@
 class Coin extends DrawableObject {
+    coinsSizes= [
+        'img/4. Marcadores/1. Coins/1.png',
+        'img/4. Marcadores/1. Coins/2.png',
+        'img/4. Marcadores/1. Coins/3.png',
+        'img/4. Marcadores/1. Coins/4.png'
+    ]
+
     constructor(x, y) {
         super();
         this.loadImage('img/4. Marcadores/1. Coins/1.png');
+        this.loadImages(this.coinsSizes);
         this.x = x;
         this.y = y;
         this.width = 50;
         this.height = 50;
+        this.animate();
+    }
+
+    animate() {
+        setInterval(() => {
+            let i = this.currentImage % this.coinsSizes.length;
+            let path = this.coinsSizes[i];
+            this.img = this.imageCache[path];
+            this.currentImage++;
+        }, 250);
     }
 }
