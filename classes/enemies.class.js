61850ddcf39bce257d23032687c66217fea2ce31
danielkrafzik <daniel.krafzik@hotmail.de>
2025-12-07 17:52:58 +0100

    added animation images for the pufferfish

--- classes/enemies.class.js
+++ classes/enemies.class.js
@@ -3,6 +3,8 @@ class Enemies extends MovableObject {
     height = 100;
     width = 100;
     energy = 40;
+
+    //PUFFER FISH IMAGES
     IMAGES_SWIMMING_PUFFER = [
         'img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/1.swim1.png',
         'img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/1.swim2.png',
@@ -10,6 +12,79 @@ class Enemies extends MovableObject {
         'img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/1.swim4.png',
         'img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/1.swim5.png'
     ];
+    IMAGES_SWIMMING_PUFFER2 = [
+        'img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/2.swim1.png',
+        'img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/2.swim2.png',
+        'img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/2.swim3.png',
+        'img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/2.swim4.png',
+        'img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/2.swim5.png'
+    ];
+    IMAGES_SWIMMING_PUFFER3 = [
+        'img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/3.swim1.png',
+        'img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/3.swim2.png',
+        'img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/3.swim3.png',
+        'img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/3.swim4.png',
+        'img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/3.swim5.png'
+    ];
+    IMAGES_TRANSITION_PUFFER = [
+        'img/2.Enemy/1.Puffer fish (3 color options)/2.Transition/1.transition1.png',
+        'img/2.Enemy/1.Puffer fish (3 color options)/2.Transition/1.transition2.png',
+        'img/2.Enemy/1.Puffer fish (3 color options)/2.Transition/1.transition3.png',
+        'img/2.Enemy/1.Puffer fish (3 color options)/2.Transition/1.transition4.png',
+        'img/2.Enemy/1.Puffer fish (3 color options)/2.Transition/1.transition5.png'
+    ];
+    IMAGES_TRANSITION_PUFFER2 = [
+        'img/2.Enemy/1.Puffer fish (3 color options)/2.Transition/2.transition1.png',
+        'img/2.Enemy/1.Puffer fish (3 color options)/2.Transition/2.transition2.png',
+        'img/2.Enemy/1.Puffer fish (3 color options)/2.Transition/2.transition3.png',
+        'img/2.Enemy/1.Puffer fish (3 color options)/2.Transition/2.transition4.png',
+        'img/2.Enemy/1.Puffer fish (3 color options)/2.Transition/2.transition5.png'
+    ];
+    IMAGES_TRANSITION_PUFFER3 = [
+        'img/2.Enemy/1.Puffer fish (3 color options)/2.Transition/3.transition1.png',
+        'img/2.Enemy/1.Puffer fish (3 color options)/2.Transition/3.transition2.png',
+        'img/2.Enemy/1.Puffer fish (3 color options)/2.Transition/3.transition3.png',
+        'img/2.Enemy/1.Puffer fish (3 color options)/2.Transition/3.transition4.png',
+        'img/2.Enemy/1.Puffer fish (3 color options)/2.Transition/3.transition5.png'
+    ];
+    IMAGES_BUBBLESWIM_PUFFER = [
+        'img/2.Enemy/1.Puffer fish (3 color options)/3.Bubbleeswim/1.bubbleswim1.png',
+        'img/2.Enemy/1.Puffer fish (3 color options)/3.Bubbleeswim/1.bubbleswim2.png',
+        'img/2.Enemy/1.Puffer fish (3 color options)/3.Bubbleeswim/1.bubbleswim3.png',
+        'img/2.Enemy/1.Puffer fish (3 color options)/3.Bubbleeswim/1.bubbleswim4.png',
+        'img/2.Enemy/1.Puffer fish (3 color options)/3.Bubbleeswim/1.bubbleswim5.png'
+    ];
+    IMAGES_BUBBLESWIM_PUFFER2 = [
+        'img/2.Enemy/1.Puffer fish (3 color options)/3.Bubbleeswim/2.bubbleswim1.png',
+        'img/2.Enemy/1.Puffer fish (3 color options)/3.Bubbleeswim/2.bubbleswim2.png',
+        'img/2.Enemy/1.Puffer fish (3 color options)/3.Bubbleeswim/2.bubbleswim3.png',
+        'img/2.Enemy/1.Puffer fish (3 color options)/3.Bubbleeswim/2.bubbleswim4.png',
+        'img/2.Enemy/1.Puffer fish (3 color options)/3.Bubbleeswim/2.bubbleswim5.png'
+    ];
+    IMAGES_BUBBLESWIM_PUFFER3 = [
+        'img/2.Enemy/1.Puffer fish (3 color options)/3.Bubbleeswim/3.bubbleswim1.png',
+        'img/2.Enemy/1.Puffer fish (3 color options)/3.Bubbleeswim/3.bubbleswim2.png',
+        'img/2.Enemy/1.Puffer fish (3 color options)/3.Bubbleeswim/3.bubbleswim3.png',
+        'img/2.Enemy/1.Puffer fish (3 color options)/3.Bubbleeswim/3.bubbleswim4.png',
+        'img/2.Enemy/1.Puffer fish (3 color options)/3.Bubbleeswim/3.bubbleswim5.png'
+    ];
+    IMAGES_DYING_PUFFER = [
+        'img/2.Enemy/1.Puffer fish (3 color options)/4.DIE/1.Dead 1 (can animate by going up).png',
+        'img/2.Enemy/1.Puffer fish (3 color options)/4.DIE/1.Dead 2 (can animate by going up).png',
+        'img/2.Enemy/1.Puffer fish (3 color options)/4.DIE/1.Dead 3 (can animate by going up).png',
+    ];
+    IMAGES_DYING_PUFFER2 = [
+        'img/2.Enemy/1.Puffer fish (3 color options)/4.DIE/2.png',
+        'img/2.Enemy/1.Puffer fish (3 color options)/4.DIE/2.3.png',
+        'img/2.Enemy/1.Puffer fish (3 color options)/4.DIE/2.2.png'
+    ];
+    IMAGES_DYING_PUFFER3 = [
+        'img/2.Enemy/1.Puffer fish (3 color options)/4.DIE/3.png',
+        'img/2.Enemy/1.Puffer fish (3 color options)/4.DIE/3.3.png',
+        'img/2.Enemy/1.Puffer fish (3 color options)/4.DIE/3.2.png'
+    ];
+
+    //YELLY FISH IMAGES
     IMAGES_SWIMMING_YELLY = [
         'img/2.Enemy/2 Jelly fish/Regular damage/Yellow 1.png',
         'img/2.Enemy/2 Jelly fish/Regular damage/Yellow 2.png',
@@ -31,6 +106,17 @@ class Enemies extends MovableObject {
         this.y = 50 + Math.random() * 250;
         this.speed = 0.15 + Math.random() * 0.5;
         this.loadImages(this.IMAGES_SWIMMING_PUFFER);  
+        this.loadImages(this.IMAGES_SWIMMING_PUFFER2);
+        this.loadImages(this.IMAGES_SWIMMING_PUFFER3);
+        this.loadImages(this.IMAGES_TRANSITION_PUFFER);
+        this.loadImages(this.IMAGES_TRANSITION_PUFFER2);
+        this.loadImages(this.IMAGES_TRANSITION_PUFFER3);
+        this.loadImages(this.IMAGES_BUBBLESWIM_PUFFER);
+        this.loadImages(this.IMAGES_BUBBLESWIM_PUFFER2);
+        this.loadImages(this.IMAGES_BUBBLESWIM_PUFFER3);
+        this.loadImages(this.IMAGES_DYING_PUFFER);
+        this.loadImages(this.IMAGES_DYING_PUFFER2);
+        this.loadImages(this.IMAGES_DYING_PUFFER3);
         this.loadImages(this.IMAGES_SWIMMING_YELLY);
         if (startImage.includes('Puffer fish')) {
             this.animatePuffer();
