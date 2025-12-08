331b882556b42393612318ab9703449f882ba2f1
danielkrafzik <daniel.krafzik@hotmail.de>
2025-12-08 23:13:36 +0100

    added images for  the yelly fish

--- classes/enemies.class.js
+++ classes/enemies.class.js
@@ -91,6 +91,48 @@ class Enemies extends MovableObject {
         'img/2.Enemy/2 Jelly fish/Regular damage/Yellow 3.png',
         'img/2.Enemy/2 Jelly fish/Regular damage/Yellow 4.png'
     ];
+    IMAGES_SWIMMING_YELLY2 = [
+        'img/2.Enemy/2 Jelly fish/Regular damage/Lila 1.png',
+        'img/2.Enemy/2 Jelly fish/Regular damage/Lila 2.png',
+        'img/2.Enemy/2 Jelly fish/Regular damage/Lila 3.png',
+        'img/2.Enemy/2 Jelly fish/Regular damage/Lila 4.png'
+    ];
+    IMAGES_TRANSITION_YELLY = [
+        'img/2.Enemy/2 Jelly fish/Súper dangerous/Green 1.png',
+        'img/2.Enemy/2 Jelly fish/Súper dangerous/Green 2.png',
+        'img/2.Enemy/2 Jelly fish/Súper dangerous/Green 3.png',
+        'img/2.Enemy/2 Jelly fish/Súper dangerous/Green 4.png'
+    ];
+    IMAGES_TRANSITION_YELLY2 = [
+        'img/2.Enemy/2 Jelly fish/Súper dangerous/Pink 1.png',
+        'img/2.Enemy/2 Jelly fish/Súper dangerous/Pink 2.png',
+        'img/2.Enemy/2 Jelly fish/Súper dangerous/Pink 3.png',
+        'img/2.Enemy/2 Jelly fish/Súper dangerous/Pink 4.png'
+    ];
+    IMAGES_DEAD_YELLY_YELLOW =[
+        'img/2.Enemy/2 Jelly fish/Dead/Yellow/y1.png',
+        'img/2.Enemy/2 Jelly fish/Dead/Yellow/y2.png',
+        'img/2.Enemy/2 Jelly fish/Dead/Yellow/y3.png',
+        'img/2.Enemy/2 Jelly fish/Dead/Yellow/y4.png'
+    ];
+    IMAGES_DEAD_YELLY_LILA =[
+        'img/2.Enemy/2 Jelly fish/Dead/Lila/L1.png',
+        'img/2.Enemy/2 Jelly fish/Dead/Lila/L2.png',
+        'img/2.Enemy/2 Jelly fish/Dead/Lila/L3.png',
+        'img/2.Enemy/2 Jelly fish/Dead/Lila/L4.png'
+    ];
+    IMAGES_DEAD_YELLY_GREEN =[
+        'img/2.Enemy/2 Jelly fish/Dead/green/g1.png',
+        'img/2.Enemy/2 Jelly fish/Dead/green/g2.png',
+        'img/2.Enemy/2 Jelly fish/Dead/green/g3.png',
+        'img/2.Enemy/2 Jelly fish/Dead/green/g4.png'
+    ];
+    IMAGES_DEAD_YELLY_PINK =[
+        'img/2.Enemy/2 Jelly fish/Dead/Pink/P1.png',
+        'img/2.Enemy/2 Jelly fish/Dead/Pink/P2.png',
+        'img/2.Enemy/2 Jelly fish/Dead/Pink/P3.png',
+        'img/2.Enemy/2 Jelly fish/Dead/Pink/P4.png'
+    ];
 
     constructor(startImage, top, right, bottom, left, startX) {
         super().loadImage(startImage);
@@ -118,6 +160,13 @@ class Enemies extends MovableObject {
         this.loadImages(this.IMAGES_DYING_PUFFER2);
         this.loadImages(this.IMAGES_DYING_PUFFER3);
         this.loadImages(this.IMAGES_SWIMMING_YELLY);
+        this.loadImages(this.IMAGES_SWIMMING_YELLY2);
+        this.loadImages(this.IMAGES_TRANSITION_YELLY);
+        this.loadImages(this.IMAGES_TRANSITION_YELLY2);
+        this.loadImages(this.IMAGES_DEAD_YELLY_YELLOW);
+        this.loadImages(this.IMAGES_DEAD_YELLY_LILA);
+        this.loadImages(this.IMAGES_DEAD_YELLY_GREEN);
+        this.loadImages(this.IMAGES_DEAD_YELLY_PINK);
         if (startImage.includes('Puffer fish')) {
             this.animatePuffer();
         } else {
